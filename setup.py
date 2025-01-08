bbad6f36434edb811c20f57f2ba8f6cc7c981deb
Kartik Pradeepan <kartikspradeepan@gmail.com>
2025-01-08 16:12:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='texture_vs_shape',
     version='0.1',
-    description='Texture vs Shape',
+    description='Texture vs shape bias analysis',
     author='Robert Geirhos',
-    url='https://github.com/rgeirhos/texture-vs-shape',
-    packages=find_packages(),
+    url='https://github.com/kartikp/texture-vs-shape',
+    packages=['models'],
+    package_dir={'models': 'models'},
+    include_package_data=True,
     install_requires=[
         'torch',
-        'torchvision'
+        'torchvision',
+        'numpy',
+        'pandas',
     ],
 )
