4cbe7072f76f92c1fd8f4e033b77ff0b5d559459
Kartik Pradeepan <kartikspradeepan@gmail.com>
2025-01-08 14:47:32 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='texture_vs_shape',
     version='0.1',
+    description='Texture vs Shape',
+    author='Robert Geirhos',
+    url='https://github.com/rgeirhos/texture-vs-shape',
     packages=find_packages(),
+    install_requires=[
+        'torch',
+        'torchvision'
+    ],
 )
